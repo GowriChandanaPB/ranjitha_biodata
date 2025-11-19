dd3c33083b370937d5b46610b6c4f29dfc52f199
Gowri Chandana PB <gowrichandana85@gmail.com>
2025-11-19 19:39:29 +0530

    Add files via upload

---
+++ webran/script.js
@@ -0,0 +1,12 @@
+Script.js
+$(document).ready(function() {
+$(".bio-card").hide().fadeIn(1000);
+$("header h1").hover(
+function() {
+$(this).css("color", "#ffd700");
+},
+function() {
+$(this).css("color", "#fff");
+}
+);
+});
